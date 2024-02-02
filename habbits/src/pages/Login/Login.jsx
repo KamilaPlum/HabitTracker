2a4381cb1ef8b877368cbcc134305b953f3a7bef
KamilaPlum <kamila.plum.boom39@gmail.com>
2024-02-02 13:18:12 -0800

    start working on creating a habit

--- habbits/src/pages/Login/Login.jsx
+++ habbits/src/pages/Login/Login.jsx
@@ -1,23 +1,72 @@
-import React from 'react';
-import {Button, Input} from "@mui/material";
-
+import React, { useState } from 'react';
+import {useNavigate} from "react-router-dom";
+import {Button, Input, TextField, Box } from "@mui/material";
+//Box  - div
+//TextField - input
+// how send data  to form -  react hook form
 const Login = () => {
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleResetError = () =>{
+        if (error) {
+            setError(null);
+        }
+    }
+
+    const handleChangeLogin = (e) => {
+        const value = e.target.value;
+        setLogin(value);
+        handleResetError();
+    };
+
+    const handleChangePassword = (e) => {
+        const value = e.target.value;
+        setPassword(value);
+        handleResetError();
+    };
+
+    const handleSubmit = () => {
+        if (login === 'test' && password === 'test') {
+            navigate('/habbits')
+        }
+        setError('Error login')
+    }
+
     return (
-        <div>
+        <form>
             <Input
                 placeholder='Your email'
-                sx={{color: 'purple',
+                sx={{
+                    color: 'purple',
                     display: 'flex',
-                    alignItems: 'center'}}
-
+                    alignItems: 'center'
+                }}
+                onChange={handleChangeLogin}
             />
             <Input
                 placeholder='Your password'
                 sx={{color: 'purple'}}
+                onChange={handleChangePassword}
             />
+            {/*condition rendering*/}
+            {
+                error && (
+                    <Box
+                        sx={{
+                            color: "red",
+                            padding: '10px'
 
+                        }}
+                    > Error</Box>
+                )
+            }
             <div>
                 <Button
+                    onClick={handleSubmit}
                     sx={{
                         color: 'purple',
                         margin: '20px 0',
@@ -29,7 +78,7 @@ const Login = () => {
                 }}
                 >Submit</Button>
             </div>
-        </div>
+        </form>
     );
 };
 
